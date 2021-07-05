10165a16a9f060adb78bd7483ea7ffd741e47676
darinkagm <dariinkagm@gmail.com>
2021-07-05 23:14:06 +0200

    meals.js updated on frontend branch

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -4,55 +4,65 @@ const knex = require("../database");
 
 router.get("/", async (request, response) => {
   try {
-
     if ("maxPrice" in request.query) {
-      const maxPrice = parseFloat(request.query.maxPrice)
-      const newPrice = await knex("meals").where("price", "<=", maxPrice)
-      response.json(newPrice)
-      return;
-    };
-
-    if (request.query.availableReservations) {
-          const getAvailableReservation = await knex("meals")
-            .select("title", "description")
-            .join("reservation", "meal.id", "=", "reservation.meal_id")
-            .where("max_reservations", ">", "number_of_guests")
-            .groupBy("reservation.meal_id");
-          response.json(getAvailableReservation);
-
-        }
-
-
-    if ("title" in request.query) {
-      const matchingTitle = request.query.title.toLocaleLowerCase()
-      const getMatchingTitle = await knex("meals").where("title", "like", `%${matchingTitle}%`)
-      response.json(getMatchingTitle)
-      return;
-    };
-
-    if ("createdAfter" in request.query) {
-      const creationDate = new Date(request.query.createdAfter)
-      const newCreationDate = await knex("meals").where("created_date", ">=", creationDate)
-      response.json(newCreationDate)
+      const maxPrice = parseFloat(request.query.maxPrice);
+      const newPrice = await knex("meals").where("price", "<=", maxPrice);
+      response.json(newPrice);
       return;
     }
 
-    if ("limit" in request.query) {
-      const mealLimit = parseFloat(request.query.limit)
-      const limitingNumber = await knex("meals").limit(mealLimit)
-      response.json(limitingNumber)
-      return;
-    } else {
-      const meals = await knex("meals")
-      response.json(meals);
-    }
+    if ("availableReservations" in request.query) {
+      let availableReservations = request.query.availableReservations;
+      if (availableReservations === "true") {
+        const filteredMeals = await knex
+          .raw(
+            `select meal.id AS meal_id, meal.title, meal.max_reservations, coalesce(SUM(reservation.number_of_guests), 0) AS total_reservations
+                from meal
+                left join reservation on meal.id = reservation.meal_id
+                group by meal.id
+                having 
+                meal.max_reservations > total_reservations`
+          )
+          .then((result) => result[0]);
 
+        response.send(filteredMeals);
+      }
+
+      if ("title" in request.query) {
+        const title = request.query.title.toLowerCase();
+        const mealByTitle = await knex("meal").where(
+          "meal.title",
+          "like",
+          "%" + title + "%"
+        );
+        response.json(mealByTitle);
+      }
+
+      if ("createdAfter" in request.query) {
+        const creationDate = new Date(request.query.createdAfter);
+        const newCreationDate = await knex("meals").where(
+          "created_date",
+          ">=",
+          creationDate
+        );
+        response.json(newCreationDate);
+        return;
+      }
+
+      if ("limit" in request.query) {
+        const mealLimit = parseFloat(request.query.limit);
+        const limitingNumber = await knex("meals").limit(mealLimit);
+        response.json(limitingNumber);
+        return;
+      } else {
+        const meals = await knex("meals");
+        response.json(meals);
+      }
+    }
   } catch (error) {
     throw error;
   }
-
 });
-
 
 router.post("/", async (request, response) => {
   try {
@@ -65,11 +75,10 @@ router.post("/", async (request, response) => {
 
 router.get("/:id", async (request, response) => {
   try {
-    const mealById = await knex("meals")
-      .where({
-        id: parseInt(request.params.id)
-      })
-    response.json(mealById)
+    const mealById = await knex("meals").where({
+      id: parseInt(request.params.id),
+    });
+    response.json(mealById);
   } catch (error) {
     throw error;
   }
@@ -77,12 +86,14 @@ router.get("/:id", async (request, response) => {
 
 router.put("/:id", async (request, response) => {
   try {
-    const updatedMealId = await knex("meals")
-      .where({
-        id: Number(request.params.id)
-      })
-      .update(request.body)
-    response.json(updatedMealId)
+    const id = parseInt(request.params.id);
+    if (isNaN(id)) {
+      response.status(404).send("User IDs should be integers.");
+      return;
+    } else {
+      const meals = await knex("meal").where({ id: id }).update(request.body);
+      response.json(meals);
+    }
   } catch (error) {
     throw error;
   }
@@ -92,9 +103,10 @@ router.delete("/:id", async (request, response) => {
   try {
     const deletedById = await knex("meals")
       .where({
-        id: request.params.id
-      }).del()
-    response.json(deletedById)
+        id: request.params.id,
+      })
+      .del();
+    response.json(deletedById);
   } catch (error) {
     throw error;
   }
